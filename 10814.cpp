bd0bd3c5b074693f4530197305c49d3ac743e665
Yeom HyeongJun <104743248+yhj0318@users.noreply.github.com>
2023-08-04 14:53:46 +0900

    Add files via upload

---
+++ 10814.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, string> a, pair<int, string> b)
+{
+    return a.first < b.first;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    pair<int, string> tmp;
+    vector<pair<int, string>> arr;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp.first >> tmp.second;
+        arr.push_back(tmp);
+    }
+    stable_sort(arr.begin(), arr.end(), compare);
+    for (int i = 0; i < n; i++)
+        cout << arr[i].first << ' ' << arr[i].second << '\n';
+}
